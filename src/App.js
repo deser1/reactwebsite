970a43008786d94fe822a14502fd043eca54f3d4
deser1 <deser1@o2.pl>
2023-10-07 02:13:06 +0200

    Create code pages and route link with navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
+import Home from './component/home/Home';
+import Portfolio from './component/portfolio/Portfolio';
+import PageNotFound from './component/404/404';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path='/' Component={Home} />
+          <Route path='/portfolio' Component={Portfolio} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>  
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
