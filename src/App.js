4fc0b65317d8d1186aaa8c824b931c9e8fd02851
deser1 <deser1@o2.pl>
2023-10-09 14:15:29 +0200

    Code css an html5 responsive and reorganize structure project file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './component/Navbar';
-import Home from './component/home/Home';
-import Portfolio from './component/portfolio/Portfolio';
-import PageNotFound from './component/404/404';
+import Navbar from './component/navbar/Navbar';
+import Home from './component/pages/home/Home';
+import Portfolio from './component/pages/portfolio/Portfolio';
+import PageNotFound from './component/pages/404/404';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Router>
-        <Routes>
-          <Route path='/' Component={Home} />
-          <Route path='/portfolio' Component={Portfolio} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Router>  
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar />
       </header>
+      <main>
+        <Router>
+          <Routes>
+            <Route path='/' Component={Home} />
+            <Route path='/portfolio' Component={Portfolio} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Router>
+      </main>
+      <footer></footer>
     </div>
   );
 }
