9a43bf92d8aba5a5bdbe71e910be203214a96f2a
deser1 <deser1@o2.pl>
2023-10-12 12:12:00 +0200

    Add API route for DB

--- src/server/index.js
+++ src/server/index.js
@@ -1,2 +1,27 @@
 const express = require('express');
 const db = require('./server/config/db.js');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3002;
+app.use(cors());
+app.use(express.json());
+
+//Route to get all project created
+app.get('/api/project/get',(req,res) =>{
+    db.query("SELECT * FROM project",(err,result)=>{
+        if (err){
+            console.log(err);
+        }
+        res.send(result)
+    })
+});
+//Route to get all skills
+app.get('/api/skills/get',(req,res) =>{
+    db.query("SELECT * FROM skills",(err,result)=>{
+        if (err){
+            console.log(err);
+        }
+        res.send(result)
+    })
+});
